d4111dad143a62eee35f7f99157ca6103c2c1f05
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 15:51:42 +0100

    Categories for news and events

--- includes/reg_taxonomies.php
+++ includes/reg_taxonomies.php
@@ -1,6 +1,6 @@
 <?php
 function create_team() {
-//create project type
+
   $labels = array(
     'name'                       => _x( 'Team', 'Taxonomy General Name', 'EAcademy' ),
     'singular_name'              => _x( 'Team', 'Taxonomy Singular Name', 'EAcademy' ),
@@ -29,5 +29,72 @@ function create_team() {
 
 
 add_action( 'init', 'create_team' );
+
+/********************* Create categories option for events  **********************/
+
+function create_categories() {
+
+  $labels = array(
+    'name'                       => _x( 'Categories', 'Taxonomy General Name', 'EAcademy' ),
+    'singular_name'              => _x( 'category', 'Taxonomy Singular Name', 'EAcademy' ),
+    'menu_name'                  => __( 'Categories', 'EAcademy' ),
+    'all_items'                  => __( 'All categories', 'EAcademy' ),
+    'new_item_name'              => __( 'New category', 'EAcademy' ),
+    'add_new_item'               => __( 'Add new category', 'EAcademy' ),
+    'edit_item'                  => __( 'Edit categories', 'EAcademy' ),
+    'update_item'                => __( 'Update categories', 'EAcademy' ),
+    'add_or_remove_items'        => __( 'Add or remove category', 'EAcademy' ),
+    'popular_items'              => __( 'Popular categories', 'EAcademy' ),
+	);
+	$args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+    'query_var'                  => 'categories',
+  );
+  register_taxonomy( 'categories', array('event'), $args );
+
+}
+
+
+add_action( 'init', 'create_categories' );
+
+
+/********************* Create categories option for news  **********************/
+
+function create_categories_news() {
+
+  $labels = array(
+    'name'                       => _x( 'Categories', 'Taxonomy General Name', 'EAcademy' ),
+    'singular_name'              => _x( 'category', 'Taxonomy Singular Name', 'EAcademy' ),
+    'menu_name'                  => __( 'Categories', 'EAcademy' ),
+    'all_items'                  => __( 'All categories', 'EAcademy' ),
+    'new_item_name'              => __( 'New category', 'EAcademy' ),
+    'add_new_item'               => __( 'Add new category', 'EAcademy' ),
+    'edit_item'                  => __( 'Edit categories', 'EAcademy' ),
+    'update_item'                => __( 'Update categories', 'EAcademy' ),
+    'add_or_remove_items'        => __( 'Add or remove category', 'EAcademy' ),
+    'popular_items'              => __( 'Popular categories', 'EAcademy' ),
+	);
+	$args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+    'query_var'                  => 'categories',
+  );
+  register_taxonomy( 'categories', array('news'), $args );
+
+}
+
+
+add_action( 'init', 'create_categories_news' );
 
  ?>
