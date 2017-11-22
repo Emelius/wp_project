b9823d474589651499332bd9a3f5d2d159c75d7d
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 10:35:26 +0100

    Styling footer

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
 </main>
 <footer>
 
-  © JSA Entrepreneurship Academy
 
-  <div id="SocialMedia">
+  <div id="socialMediaOuter">
     <?php $uri =  get_template_directory_uri() ?>
-    <div id="icon"><img src="<?php echo $uri ?>/images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p>
-    <div id="icon"><img src="<?php echo $uri ?>/images/instaicon.png"></div><p>jibs_entrepreneurship</p>
-    <div id="icon"><img src="<?php echo $uri ?>/images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p>
+    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p></div>
+    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/instaicon.png"></div><p>jibs_entrepreneurship</p></div>
+    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p></div>
   </div>
+
+  <h6>© JSA Entrepreneurship Academy</h6>
 
 </footer>
 <?php wp_footer()?>
