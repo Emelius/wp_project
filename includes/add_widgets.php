2471dc6ad23342149c8b1f5da17f0edfd3f38eab
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 10:16:30 +0100

    Update

--- includes/add_widgets.php
+++ includes/add_widgets.php
@@ -3,9 +3,9 @@
   $args = array(
     'id'            => 'page-sidebar',
     /** Visible name in the Admin Dashboard Widget page */
-    'name'          => __( 'Page sidebar', 'portfolio_theme' ),
+    'name'          => __( 'Page sidebar', 'EAcademy' ),
     /** Visible description in the Admin Dashboard Widget page */
-    'description'   => __( 'Show my skills and contact info', 'portfolio_theme' ),
+    'description'   => __( 'A page sidebar', 'EAcademy' ),
 
     // /** HTML to wrap widget title in  */
     // 'before_title'  => '<p class="widget-title">',
@@ -15,25 +15,6 @@
     // 'after_widget'  => '</section>',
   );
   register_sidebar( $args );
-
-  //second widget
-  $args2 = array(
-    'id'            => 'project-sidebar',
-    /** Visible name in the Admin Dashboard Widget page */
-    'name'          => __( 'Project sidebar', 'portfolio_theme' ),
-    /** Visible description in the Admin Dashboard Widget page */
-    'description'   => __( 'Show the skills for specific project and social links', 'portfolio_theme' ),
-  );
-  register_sidebar( $args2 );
-
-  $args3 = array(
-    'id'            => 'site-description',
-    /** Visible name in the Admin Dashboard Widget page */
-    'name'          => __( 'Site description', 'portfolio_theme' ),
-    /** Visible description in the Admin Dashboard Widget page */
-    'description'   => __( 'Show a short description of the site', 'portfolio_theme' ),
-  );
-  register_sidebar( $args3 );
 
  }
  add_action( 'widgets_init', 'add_widgets' );
