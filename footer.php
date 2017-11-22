0689219941be58c969c909e7a44cde1447d07f90
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 09:04:10 +0100

    New files

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+
+  © JSA Entrepreneurship Academy
+</footer>
+<?php wp_footer()?>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>E Academy</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
+    <?php wp_head(); ?>
+  </head>
+  <body>
