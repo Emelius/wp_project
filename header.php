fa64180a295437cf347d52193c79db77306f542f
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 09:37:21 +0100

    logo and nav
    logo support and navigation

--- header.php
+++ header.php
@@ -7,3 +7,28 @@
     <?php wp_head(); ?>
   </head>
   <body>
+
+    <div id="header_wrap">
+        <header>
+
+          <div class='logo'>
+            <?php
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+            if ( has_custom_logo() ) {
+                    echo '<img src="'. esc_url( $logo[0] ) .'">';
+            } else {
+                    echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+            }
+            ?>
+          </div>
+
+
+          <nav>
+            <ul>
+            <?php wp_nav_menu( array( 'theme_location' => 'header-nav' ) );?>
+            </ul>
+          </nav>
+        </header>
+
+    </div>
