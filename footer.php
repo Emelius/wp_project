316d6bac9f5906a4d753332225ae0ad7830c9186
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 10:02:04 +0100

    Update
    icons

--- footer.php
+++ footer.php
@@ -2,6 +2,13 @@
 <footer>
 
   © JSA Entrepreneurship Academy
+
+  <div id="SocialMedia">
+    <div id="icon"><img src="../images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p>
+    <div id="icon"><img src="../images/instaicon.png"></div><p>jibs_entrepreneurship</p>
+    <div id="icon"><img src="../images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p>
+  </div>
+
 </footer>
 <?php wp_footer()?>
 </div>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>E Academy</title>
+    <title>Entrepreneurship Academy</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,400i,600,600i,700,700i,800" rel="stylesheet">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
     <?php wp_head(); ?>
