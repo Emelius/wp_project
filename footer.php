f2571756a2f5ce7055cdc5329e252ff40ec07a0f
Fractive <Fractive@live.com>
2017-11-22 11:37:15 +0100

    Footern ish-klart

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
   <div id="socialMediaOuter">
     <?php $uri =  get_template_directory_uri() ?>
-    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p></div>
-    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/instaicon.png"></div><p>jibs_entrepreneurship</p></div>
-    <div class="socialInnerDiv"><div class="icon"><img src="<?php echo $uri ?>/images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p></div>
+    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/facebookicon.png"><p>JSA Entrepreneurship Academy</p></div>
+    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/instaicon.png"><p>jibs_entrepreneurship</p></div>
+    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/emailicon.png"><p>entrepreneurship.jsa@js.ju.se</p></div>
   </div>
 
   <h6>© JSA Entrepreneurship Academy</h6>
