268adf8cda1a03d40943a8ed2de8e22a32295a52
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-22 10:04:11 +0100

    cant find files

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
   © JSA Entrepreneurship Academy
 
   <div id="SocialMedia">
-    <div id="icon"><img src="../images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p>
-    <div id="icon"><img src="../images/instaicon.png"></div><p>jibs_entrepreneurship</p>
-    <div id="icon"><img src="../images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p>
+    <div id="icon"><img src="images/facebookicon.png"></div><p>JSA Entrepreneurship Academy</p>
+    <div id="icon"><img src="images/instaicon.png"></div><p>jibs_entrepreneurship</p>
+    <div id="icon"><img src="images/emailicon.png"></div><p>entrepreneurship.jsa@js.ju.se</p>
   </div>
 
 </footer>
