e7fc829701208e331b76217673eb954b693df48b
Fractive <Fractive@live.com>
2017-11-23 12:10:44 +0100

    News and events

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 <div class="bigLogo">
-    <?php $uri =  get_template_directory_uri() ?>
+    <?php $uri = get_template_directory_uri() ?>
 
     <img class="logoBackground" src="<?php echo $uri ?>/images/groupPhoto.jpg">
 
@@ -18,6 +18,47 @@ get_header();
         }
     ?>
 </div>
+
+<?php
+    function showcase($item) {
+        $args = array(
+            "post_type" => $item,
+            "posts_per_page" => 4
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                ?><div class="item"><?php
+                    ?><a href="<?php the_permalink() ?>"><?php the_post_thumbnail("grid_thumbnail"); ?></a><?php
+                    ?><div class="text"><?php
+                        ?><h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3><?php
+                        the_excerpt();
+                    ?></div><?php
+                ?></div><?php
+            }
+        }
+    }
+?>
+
+<section id="about">
+    <h2>About Entrepreneurship Academy</h2>
+    <p>wwwwwwwwwwwwww</p>
+</section>
+<section id="news">
+    <h2>News</h2>
+    <?php
+        showcase("news");
+    ?>
+</section>
+<section id="events">
+    <h2>Events</h2>
+    <?php
+        showcase("event");
+    ?>
+</section>
 
 <?php
 get_footer();
