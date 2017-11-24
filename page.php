7b09023f491ce8ca92bb766c54c2c92e4b9c6e8e
Emelius <32455107+Emelius@users.noreply.github.com>
2017-11-24 11:33:01 +0100

    the loop

--- page.php
+++ page.php
@@ -3,6 +3,18 @@ get_header();
 ?>
 
 <?php
+  if( have_posts() ) {
+    while ( have_posts() ){
+        the_post(); ?>
+        <h2><?php the_title();?></h2><?php
+        the_content();
+
+      }
+    }
+?>
+
+
+<?php
     $terms = get_terms(array(
         "taxonomy" => "team"
     ));
