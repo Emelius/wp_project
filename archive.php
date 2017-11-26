d9730cc25c58269627ca4fbbae1ec0410d88271f
JGranlund <granlund.johannes@gmail.com>
2017-11-26 21:44:08 +0100

    image size

--- archive.php
+++ archive.php
@@ -6,10 +6,9 @@ archive.php
   if( have_posts() ) {
     while ( have_posts() ){
         the_post(); ?>
-        <h2><?php the_title();?></h2><?php
-        the_content();?>
+        <h2><?php the_title();?></h2>
         <a href="<?php the_permalink() ?>"><?php the_post_thumbnail("grid_thumbnail"); ?></a><?php
-
+        the_excerpt();
       }
     }
 ?>
--- includes/add_image_size.php
+++ includes/add_image_size.php
@@ -1,5 +1,5 @@
 <?php
 //defines the size of the thumbnails
-add_image_size( 'grid_thumbnail', 300, 300, true );
+add_image_size( 'grid_thumbnail', 150, 150, true );
 add_image_size( 'single_large', 660, 400, false);
 ?>
