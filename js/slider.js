28fc30c376df9c7c7335a0249084edbf543f86d6
Fractive <Fractive@live.com>
2017-11-27 17:47:32 +0100

    Slider with controls

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,66 @@
-var items = document.querySelectorAll("#news .item");
+$(document).ready(function() {
+    var arrows = document.getElementsByClassName("sliderArrow");
+    var animating = false;
+    var date = new Date();
+    var counter = date.getTime();
+    var speed = 1000;
 
-var slideCounter = 0;
+    for (var i = 0; i < arrows.length; i++) {
+        $(arrows[i]).click(function(e) {
+            e.preventDefault();
+            date = new Date();
 
-function slide() {
-    var exit = items[slideCounter];
-    var enter = items[(slideCounter + 1) % items.length];
-    enter.style.left = "-100%";
+            if (counter + speed < date.getTime()) {
+                if (this.id == "previous") {
+                    slide(slideCounter);
+                } else if (this.id == "next") {
+                    slide(slideCounter, "reverse");
+                }
+            }
+        });
+    }
 
-    $(enter).animate({
-        left: "50%"
-    }, 2000, function() {});
+    var items = document.querySelectorAll("#news .item");
 
-    $(exit).animate({
-        left: "200%"
-    }, 2000, function() {});
+    var slideCounter = 0;
 
-    slideCounter = (slideCounter + 1) % items.length;
-}
+    slide(slideCounter);
 
-setInterval(slide, 2500);
+    function slide(id, direction) {
+        if (document.hasFocus()) {
+            if (direction == "reverse") {
+                var exit = items[id];
+                var enter = items[(items.length + id - 1) % items.length];
+                enter.style.left = "200%";
+
+                $(enter).animate({
+                    left: "50%"
+                }, speed, function() {});
+
+                $(exit).animate({
+                    left: "-100%"
+                }, speed, function() {});
+
+                slideCounter = (items.length + slideCounter - 1) % items.length;
+            } else {
+                var exit = items[id];
+                var enter = items[(id + 1) % items.length];
+                enter.style.left = "-100%";
+
+                $(enter).animate({
+                    left: "50%"
+                }, speed, function() {});
+
+                $(exit).animate({
+                    left: "200%"
+                }, speed, function() {});
+
+                slideCounter = (slideCounter + 1) % items.length;
+            }
+
+            counter = date.getTime();
+        }
+    }
+
+    setInterval(function() {slide(slideCounter)}, 20000);
+});
