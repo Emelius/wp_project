e5a2776a7151642e99805741d7d3009fd2383c25
JGranlund <granlund.johannes@gmail.com>
2017-11-28 12:49:40 +0100

    old version, probalb ywill break stuff

--- singular.php
+++ singular.php
@@ -13,22 +13,43 @@
         </div>
 
       </div>
-    <section>
+    <section class="article_section">
       <article>
         <h1 class="single_h1"><?php the_title();?></h1>
         <p class="article_body">
           <?php the_content(); ?>
         </p>
         <hr class="bottom_line"/>
-        <div class="author">
-          <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?>
-          Author: <?php the_author(); ?>
-          </div>
       </article>
 
       <aside class="sidelist">
         <h2>More <?php echo $post_type; ?> </h2>
+        <div class="more">
+        <?php
+        function showcase($item) {
+            $args = array(
+                "post_type" => $item,
+                "posts_per_page" => 4
+            );
 
+            $query = new WP_Query($args);
+
+            if ($query->have_posts()) {
+                while ($query->have_posts()) {
+                    $query->the_post();
+                    ?><div class="item"><?php
+                        ?><a href="<?php the_permalink() ?>"><?php the_post_thumbnail("grid_thumbnail"); ?></a><?php
+                        ?><div class="text"><?php
+                            ?><h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3><?php
+                            the_excerpt();
+                        ?></div><?php
+                    ?></div><?php
+                }
+            }
+        }
+        showcase($post_type);
+         ?>
+        </div>
       </aside>
     </section>
 
