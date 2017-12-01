e187e0237e074b560feb3ffd8e46c940e372b25a
Emelius <32455107+Emelius@users.noreply.github.com>
2017-12-01 14:04:47 +0100

    New fonts

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Entrepreneurship Academy</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,400i,600,600i,700,700i,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700" rel="stylesheet">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <?php wp_head(); ?>
