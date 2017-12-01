985c7b22cd3f4c179afde7e1abb2b7a1d5f49ce7
Emelius <32455107+Emelius@users.noreply.github.com>
2017-12-01 14:54:16 +0100

    Changed footer

--- footer.php
+++ footer.php
@@ -6,10 +6,9 @@
     <?php $uri = get_template_directory_uri() ?>
     <div class="socialInnerDiv"><a href="https://www.facebook.com/JSAEntrepreneurshipAcademy/?ref=br_rs"><img class="icon" src="<?php echo $uri ?>/images/facebookicon.png"></a></div>
     <div class="socialInnerDiv"><a href="https://www.instagram.com/jibs_entrepreneurship/"><img class="icon" src="<?php echo $uri ?>/images/instaicon.png"></a></div>
-    <div class="socialInnerDiv"><a><img class="icon" src="<?php echo $uri ?>/images/emailicon.png"></a></div>
   </div>
 
-  <h6>© JSA Entrepreneurship Academy</h6>
+  <p>© JSA Entrepreneurship Academy</p>
 
 </footer>
 <?php wp_footer()?>
