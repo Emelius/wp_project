163c7a8e4427dab236881cb5ac2e07e5666fdff8
Emelius <32455107+Emelius@users.noreply.github.com>
2017-12-01 14:30:08 +0100

    bla ugly

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
   <div id="socialMediaOuter">
     <?php $uri = get_template_directory_uri() ?>
-    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/facebookicon.png"><a href="https://www.facebook.com/JSAEntrepreneurshipAcademy/?ref=br_rs">JSA Entrepreneurship Academy</a></div>
-    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/instaicon.png"><a href="https://www.instagram.com/jibs_entrepreneurship/">jibs_entrepreneurship</a></div>
-    <div class="socialInnerDiv"><img class="icon" src="<?php echo $uri ?>/images/emailicon.png"><a>entrepreneurship.jsa@js.ju.se</a></div>
+    <div class="socialInnerDiv"><a href="https://www.facebook.com/JSAEntrepreneurshipAcademy/?ref=br_rs"><img class="icon" src="<?php echo $uri ?>/images/facebookicon.png"></a></div>
+    <div class="socialInnerDiv"><a href="https://www.instagram.com/jibs_entrepreneurship/"><img class="icon" src="<?php echo $uri ?>/images/instaicon.png"></a></div>
+    <div class="socialInnerDiv"><a><img class="icon" src="<?php echo $uri ?>/images/emailicon.png"></a></div>
   </div>
 
   <h6>© JSA Entrepreneurship Academy</h6>
