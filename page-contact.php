459fef26d527036cc7c6b26b43f4e832c5a404d5
JGranlund <granlund.johannes@gmail.com>
2017-12-01 15:57:29 +0100

    asdf

--- page-contact.php
+++ page-contact.php
@@ -7,15 +7,19 @@ get_header();
   if( have_posts() ) {
     while ( have_posts() ){
         the_post(); ?>
-        <h2><?php the_title();?></h2>
-        <p></p><?php
-        the_field('text'); ?>
-        </p><img src="<?php $image = get_field('image'); $thumb = $image['sizes'][ 'wide_thumbnail' ]; echo $thumb; ?>">
+        <div class="left_box">
+          <h2><?php the_title();?></h2>
+          <img src="<?php $image = get_field('image'); $thumb = $image['sizes'][ 'big_square' ]; echo $thumb; ?>">
+          <p class="text_box"><?php
+          the_field('text'); ?></p>
+        </div>
         <?php
       }
-    }
-?>
+    }?>
+    <div class="right_box">
+      <?php get_template_part('partials/form');?>
 
+    </div>
 <?php
 get_footer();
 ?>
