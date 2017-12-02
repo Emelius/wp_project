8bbfc45124148ec0596807040264ceed7f22568e
Emelius <32455107+Emelius@users.noreply.github.com>
2017-12-02 22:05:15 +0100

    Styling edits

--- page-contact.php
+++ page-contact.php
@@ -2,14 +2,14 @@
     get_header();
 ?>
 
-<section>
+<section id="contactContent">
     <?php
       if( have_posts() ) {
         while ( have_posts() ){
             the_post(); ?>
             <div class="left_box">
               <h2><?php the_title();?></h2>
-              <img src="<?php $image = get_field('image'); $thumb = $image['sizes'][ 'big_square' ]; echo $thumb; ?>">
+              <img src="<?php $image = get_field('image'); $thumb = $image['sizes'][ 'grid_thumbnail' ]; echo $thumb; ?>">
               <p class="text_box"><?php
               the_field('text'); ?></p>
             </div>
