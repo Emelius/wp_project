8395cc0b57ef915bc4b093c6a010a2ba9a7053f8
Emelius <32455107+Emelius@users.noreply.github.com>
2017-12-02 23:13:11 +0100

    remove comments

--- includes/remove_post_post_type.php
+++ includes/remove_post_post_type.php
@@ -2,8 +2,15 @@
 
 add_action('admin_menu','remove_default_post_type');
 
+
 function remove_default_post_type() {
 	remove_menu_page('edit.php');
 }
+
+function remove_comments() {
+	remove_menu_page( 'edit-comments.php' );
+}
+
+add_action('admin_menu','remove_comments');
 
 ?>
