d106e06ebebb88284c20a406ea44a66103c7e43a
Fractive <Fractive@live.com>
2017-12-02 20:29:11 +0100

    Page creation fix

--- includes/fix_title.php
+++ includes/fix_title.php
@@ -1,20 +1,22 @@
 <?php
     function updateTitle($post_id) {
-        $post_type = get_post_type($post_id);
+        if ($post_type == "page") {
+            $post_type = get_post_type($post_id);
 
-        if ($post_type == "members") {
-            $field = "name";
-        } else {
-            $field = "title";
+            if ($post_type == "members") {
+                $field = "name";
+            } else {
+                $field = "title";
+            }
+
+            $post = array(
+                "ID" => $post_id,
+                "post_title" => get_field($field, $post_id),
+                "post_name" => get_field($field, $post_id)
+            );
+
+            wp_update_post($post);
         }
-
-        $post = array(
-            "ID" => $post_id,
-            "post_title" => get_field($field, $post_id),
-            "post_name" => get_field($field, $post_id)
-        );
-
-        wp_update_post($post);
     }
 
     add_action("acf/save_post", "updateTitle", 20);
