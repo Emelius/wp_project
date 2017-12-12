f103104c057bada245f990fafa8c75480dfc7898
JGranlund <granlund.johannes@gmail.com>
2017-12-12 15:25:34 +0100

    Contact styling and contact form widget added

--- includes/add_widgets.php
+++ includes/add_widgets.php
@@ -16,6 +16,21 @@
   );
   register_sidebar( $args );
 
+  $args2 = array(
+      'id'            => 'contact',
+      /** Visible name in the Admin Dashboard Widget page */
+      'name'          => __( 'Contact form', 'EAcademy' ),
+      /** Visible description in the Admin Dashboard Widget page */
+      'description'   => __( 'text widget for the contact form', 'EAcademy' ),
+
+      // /** HTML to wrap widget title in  */
+      // 'before_title'  => '<p class="widget-title">',
+      // 'after_title'   => '</p>',
+      // /** HTML to wrap each widget  */
+      // 'before_widget' => '<section>',
+      // 'after_widget'  => '</section>',
+    );
+    register_sidebar( $args2 );
  }
  add_action( 'widgets_init', 'add_widgets' );
 ?>
